28d37d08db7407b6dfc213b256e094b98a7b1fb2
Lars Krantz <lars.krantz@alaz.se>
2018-02-05 12:29:55 +0100

    Initial commit, bare mix.new

---
+++ lib/logger_appsignal_backend.ex
@@ -0,0 +1,18 @@
+defmodule LoggerAppsignalBackend do
+  @moduledoc """
+  Documentation for LoggerAppsignalBackend.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoggerAppsignalBackend.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logger_appsignal_backend_test.exs
@@ -0,0 +1,8 @@
+defmodule LoggerAppsignalBackendTest do
+  use ExUnit.Case
+  doctest LoggerAppsignalBackend
+
+  test "greets the world" do
+    assert LoggerAppsignalBackend.hello() == :world
+  end
+end
